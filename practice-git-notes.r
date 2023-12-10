c59f6c04bb1ebfbb8de189f267197f93b980a31d
Emmanuel Maduneme <114446945+emaduneme@users.noreply.github.com>
2023-12-09 20:03:23 -0800

    Kaggle Notebook | Practice GIT Notes | Version 3

---
+++ practice-git-notes.r
@@ -0,0 +1,123 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "870517a7",
+   "metadata": {
+    "papermill": {
+     "duration": 0.002959,
+     "end_time": "2023-12-10T04:03:11.310676",
+     "exception": false,
+     "start_time": "2023-12-10T04:03:11.307717",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5f063029",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001838,
+     "end_time": "2023-12-10T04:03:11.314610",
+     "exception": false,
+     "start_time": "2023-12-10T04:03:11.312772",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "id": "1380dc26",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "papermill": {
+     "duration": 0.002192,
+     "end_time": "2023-12-10T04:03:11.318748",
+     "exception": false,
+     "start_time": "2023-12-10T04:03:11.316556",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "# This R environment comes with many helpful analytics packages installed\n",
+    "# It is defined by the kaggle/rstats Docker image: https://github.com/kaggle/docker-rstats\n",
+    "# For example, here's a helpful package to load\n",
+    "\n",
+    "library(tidyverse) # metapackage of all tidyverse packages\n",
+    "\n",
+    "# Input data files are available in the read-only \"../input/\" directory\n",
+    "# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory\n",
+    "\n",
+    "\n",
+    "\n",
+    "# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using \"Save & Run All\" \n",
+    "# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "46257330",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001786,
+     "end_time": "2023-12-10T04:03:11.322542",
+     "exception": false,
+     "start_time": "2023-12-10T04:03:11.320756",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "Random Stuff"
+   ]
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "isGpuEnabled": false,
+   "isInternetEnabled": true,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.729233,
+   "end_time": "2023-12-10T04:03:11.446049",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2023-12-10T04:03:07.716816",
+   "version": "2.5.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
